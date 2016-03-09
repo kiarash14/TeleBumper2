b5e6f56ad5a2f829746027cdfc71057683e660d4
maysam0111 <madamp90@gmail.com>
2016-03-09 14:31:46 +0330

    Added files via upload

--- plugins/invite.lua
+++ plugins/invite.lua
@@ -1,84 +1,44 @@
---[[
-Invite other user to the chat group.
-Use !invite ********* (where ********* is id_number) to invite a user by id_number.
-This is the most reliable method.
-Use !invite @username to invite a user by @username.
-Less reliable. Some users don't have @username.
-Use !invite Type print_name Here to invite a user by print_name.
-Unreliable. Avoid if possible.
-]]--
-
 do
-
--- Think it's kind of useless. Just to suppress '*** lua: attempt to call a nil value'
-local function callback(extra, success, result)
-  if success == 1 and extra ~= false then
-    return extra.text
-  else
-    return send_large_msg(chat, "Can't invite user to this group.")
-  end
+local function callbackres(extra, success, result)
+  local user = 'user#id'..result.peer_id
+	local chat = 'chat#id'..extra.chatid
+	local channel = 'channel#id'..extra.chatid
+	if is_banned(result.id, extra.chatid) then 
+        send_large_msg(chat, 'User is banned.')
+        send_large_msg(channel, 'User is banned.')
+	elseif is_gbanned(result.id) then
+	    send_large_msg(chat, 'User is globaly banned.')
+		send_large_msg(channel, 'User is globaly banned.')
+	else    
+	    chat_add_user(chat, user, ok_cb, false) 
+		channel_invite(channel, user, ok_cb, false)
+	end
 end
-
-local function resolve_username(extra, success, result)
-  local chat = extra.chat
-  if success == 1 then
-    local user = 'user#id'..result.id
-    chat_add_user(chat, user, callback, false)
-    return extra.text
-  else
-    return send_large_msg(chat, "Can't invite user to this group.")
+function run(msg, matches)
+  local data = load_data(_config.moderation.data)
+  if not is_momod(msg) then
+	return
   end
-end
-
-local function action_by_reply(extra, success, result)
-  if success == 1 then
-    chat_add_user('chat#id'..result.to.id, 'user#id'..result.from.id, callback, false)
-  else
-    return send_large_msg('chat#id'..result.to.id, "Can't invite user to this group.")
+  if not is_admin1(msg) then -- For admins only !
+		return 'Only admins can invite.'
   end
-end
-
-local function run(msg, matches)
-  local user_id = matches[1]
-  local chat = 'chat#id'..msg.to.id
-  local text = "Add: "..user_id.." to "..chat
-  if msg.to.type == 'chat' then
-    if msg.reply_id and msg.text == "!invite" then
-      msgr = get_message(msg.reply_id, action_by_reply, {msg=msg})
+  if not is_realm(msg) then
+    if data[tostring(msg.to.id)]['settings']['lock_member'] == 'yes' and not is_admin1(msg) then
+		  return 'Group is private.'
     end
-    if string.match(user_id, '^%d+$') then
-      user = 'user#id'..user_id
-      chat_add_user(chat, user, callback, {chat=chat, text=text})
-    elseif string.match(user_id, '^@.+$') then
-      username = string.gsub(user_id, '@', '')
-      msgr = res_user(username, resolve_username, {chat=chat, text=text})
-    else
-      user = string.gsub(user_id, ' ', '_')
-      chat_add_user(chat, user, callback, {chat=chat, text=text})
-    end
-  else
-    return 'This isnt a chat group!'
   end
+	if msg.to.type ~= 'chat' or msg.to.type ~= 'channel' then 
+		local cbres_extra = {chatid = msg.to.id}
+		local username = matches[1]
+		local username = username:gsub("@","")
+		resolve_username(username,  callbackres, cbres_extra)
+	end
 end
-
 return {
-  description = 'Invite other user to the chat group.',
-  usage = {
-    -- Need space in front of this, so bot won't consider it as a command
-    ' !invite [id|user_name|name]'
-  },
-  patterns = {
-    "^!invite$",
-    "^!invite (.*)$",
-    "^!invite (%d+)$"
-  },
-  run = run,
-  privileged = true
+    patterns = {
+      "^[#!/]invite (.*)$"
+    },
+    run = run
 }
 
 end
-
---Copyright; @behroozyaghi
---Persian Translate; @behroozyaghi
---ch : @nod32team
---کپی بدون ذکر منبع حرام است
--- plugins/onservice.lua
+++ plugins/onservice.lua
@@ -1,26 +1,23 @@
 do
 -- Will leave the group if be added
 local function run(msg, matches)
-local bot_id = 182195776 -- your bot id
-   -- like local bot_id = 1234567
-    if matches[1] == 'leavenod32' and is_admin(msg) then
+local bot_id = our_id 
+local receiver = get_receiver(msg)
+    if matches[1] == 'leave' and is_admin1(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
-    elseif msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
-      send_large_msg("chat#id"..msg.to.id, 'this is not one of my groups fuck you (coming soon u Globall Banned and Blocked of this bot KOSE NANAT❤️).', ok_cb, false)
-      chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
-      block_user("user#id"..msg.from.id,ok_cb,false)
+	   leave_channel(receiver, ok_cb, false)
+    elseif msg.service and msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_admin1(msg) then
+       send_large_msg(receiver, 'This is not one of my groups.', ok_cb, false)
+       chat_del_user(receiver, 'user#id'..bot_id, ok_cb, false)
+	   leave_channel(receiver, ok_cb, false)
     end
 end
  
 return {
   patterns = {
-    "^[!/](leavenod32)$",
+    "^[#!/](leave)$",
     "^!!tgservice (.+)$",
   },
   run = run
 }
 end
---Copyright and edit; @behroozyaghi
---Persian Translate; @behroozyaghi
---ch : @nod32team
---کپی بدون ذکر منبع حرام است
