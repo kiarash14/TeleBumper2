2c901db430196120f164cc874156ebfc0a315351
kiarash14 <omidbakhsh22@gmail.com>
2016-03-09 23:22:24 -0800

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -228,29 +228,21 @@ function create_config( )
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {100096055,0,tonumber(our_id)},--Sudo users
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v4
-An advanced administration bot based on TG-CLI written in Lua
-
-https://github.com/SEEDTEAM/TeleSeed
-
-Admins
-@Empero [Founder]
-@imandaneshi [Developer]
-@POTUS [Developer]
-@seyedan25 [Manager]
-@aRandomStranger [Admin]
-
-Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
-
+    about_text = [[TeleBumper v5 
+-----------------------    
+ http://s7.picofile.com/file/8234722668/Bumper  
+----------------------- 
+Sudo:  
+@kiarash_gh1 [Manager]  
+-----------------------    
+Id Bot  
+@TeleBumper  
+-----------------------
 Our channels
-@teleseedch [English]
-@iranseed [persian]
+@TeleBumperch [English]
+-----------------------
 ]],
     help_text_realm = [[
 Realm Commands:
